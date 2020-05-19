98ad59706735a55c39d0e4de2f832b58b18749b6
Antti Leinonen <antti.leinonen@helsinki.fi>
2020-05-19 19:54:07 +0300

    Add bin-directory to update-material-template.sh

--- bin/update-material-template.sh
+++ bin/update-material-template.sh
@@ -13,16 +13,22 @@ fi
 updated_repository=${1%/}
 
 BASEDIR=$(dirname "$0")
-BASEDIR_WITHOUT_BIN=${BASEDIR%/bin}
+BASEDIR_WITHOUT_BIN=${BASEDIR%bin}
+
+if [ -z "$BASEDIR_WITHOUT_BIN" ]; then
+  BASEDIR_WITHOUT_BIN="."
+fi
 
 rm -r $updated_repository/gatsby-*
 rm -r $updated_repository/package*
 rm -r $updated_repository/plugins
 rm -r $updated_repository/src
 rm -r $updated_repository/docs
+rm -r $updated_repository/bin
 
 cp -r $BASEDIR_WITHOUT_BIN/gatsby-* $updated_repository
 cp -r $BASEDIR_WITHOUT_BIN/package* $updated_repository
 cp -r $BASEDIR_WITHOUT_BIN/plugins $updated_repository/plugins
 cp -r $BASEDIR_WITHOUT_BIN/src $updated_repository/src
 cp -r $BASEDIR_WITHOUT_BIN/docs $updated_repository/docs
+cp -r $BASEDIR_WITHOUT_BIN/bin $updated_repository/bin
